d733bffe9e4af5e0c3d2d6909c4d13b721323cae
Danillo.sdd <danillo.sdd@hotmail.com>
2023-09-10 12:17:06 -0300

    update

--- Joyclass/Introdução à Programação/tratamento_de_excecoes/tratamento_de_excecoes.py
+++ Joyclass/Introdução à Programação/tratamento_de_excecoes/tratamento_de_excecoes.py
@@ -13,4 +13,22 @@ Caso o usuário não digite um número ou apareça um inválido no campo do ano,
 Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
 """
 
-a
+nome = input("Informe seu Nome Completo: ")
+
+ano_valido = False
+
+while(ano_valido == False):
+  try:
+
+    ano_nascimento = int(input("Informe o ano do seu nascimento(Entre 1922 e 2021): "))
+
+
+    if(ano_nascimento >= 1922) and (ano_nascimento <= 2021):
+      print("\nO nome do usuário é: " + nome)
+      print("Neste ano de 2022 sua idade que completou, ou completará é de: " + str((2022  - ano_nascimento)))
+      ano_valido = True
+    else:
+      print("\nAno de nascimento precisa estar entre 1922 e 2021!")
+
+  except:
+    print("\nAno de nascimento inválido!")
